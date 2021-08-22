9747d372281e35122b64336e3c9ed4f5a18e5d84
Dewangga P Praja <dewzzpro@gmail.com>
2021-08-22 19:17:29 +0700

    add java script file

---
+++ start.js
@@ -0,0 +1,22 @@
+console.log("test");
+
+var nama = "Dewangga";
+var umur = 25;
+var lakiLaki = true;
+
+if (!lakiLaki) {
+    console.log("saya perempuan"); 
+} else {
+    console.log("saya laki laki"); 
+}
+
+var daftarNama = [
+    "Dewangga",
+    "Prasetya",
+    "Praja",
+]
+
+var panjang = daftarNama.length
+daftarNama.push("Tambahan")
+
+console.log(daftarNama[3])
