e487f0b035d7619eb86e74e64f94356c92d134ab
Dewangga P Praja <dewzzpro@gmail.com>
2021-08-27 16:46:20 +0700

    upload how to git update start.js: for loop pattern

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 console.log("test");
 
+// variable
 var nama = "Dewangga";
 var umur = 25;
 var lakiLaki = true;
@@ -20,3 +21,16 @@ var panjang = daftarNama.length
 daftarNama.push("Tambahan")
 
 console.log(daftarNama[3])
+
+// 1. loop variable
+// 2. end value
+// 3. loop operation
+for (var i = 0; i < 5; i++) {
+    console.log(i + " loop");
+}
+
+var i = 0;
+while (i < 5) {
+    console.log(i + " loop2");
+    i++
+}
