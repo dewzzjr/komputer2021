b4a81722c064c51b8111e46c79bd747f0f047083
Dewangga P Praja <dewzzpro@gmail.com>
2021-09-04 17:21:17 +0700

    double for loop on js and other example for loop

---
+++ coba.py
@@ -0,0 +1,4 @@
+loop = [1,2,3,4,5] # array
+for i in loop:
+    star = "*" * i # memanfaatkan elemen yg ada dalam array
+    print(star)
--- start.js
+++ start.js
@@ -23,9 +23,9 @@ daftarNama.push("Tambahan")
 console.log(daftarNama[3])
 
 // 1. loop variable
-// 2. end value
+// 2. end value / syarat perulangan
 // 3. loop operation
-for (var i = 0; i < 5; i++) {
+for (var i = 5; i > 0; i--) {
     console.log(i + " loop");
 }
 
@@ -34,3 +34,79 @@ while (i < 5) {
     console.log(i + " loop2");
     i++
 }
+
+var j = 5
+var loop = true
+while (loop) {
+    j--;
+    console.log(loop + " "+ j + " loop3");
+    if (j < 0) {
+        loop = false;
+    }
+}
+
+// kesalahan dalam membuat suatu perulangan
+// infinite loop
+// untuk mengubah suatu loop agar dia jadi valid
+// maka loop variable harus dibandingkan dengan syarat perulangannya
+// jika bisa true maka loop minimal jalan sekali
+// apakah loop variable mendekati syarat perulangan atau menjauhi
+// apabila menjauhi maka akan terjadi infinite loop, dan harus dihindari
+
+// FUNCTION
+function tambah(a, b) {
+    return a+b;
+}
+
+// VARIABLE
+// 1. declaration & assignment
+var hasil = tambah(1,3);
+console.log(hasil);
+
+// 2. assignment
+hasil = tambah(2,5);
+console.log(hasil);
+
+var kali = function(a, b){
+    return a*b;
+}
+
+// 3. declaration only
+hasil2 = kali(2,5);
+console.log(hasil2);
+
+hasil2 = kali(3,6)
+console.log(hasil2);
+
+// loop luar 1
+// i = 0
+// false
+// hasil *
+
+// loop luar 2
+// i = 1
+// j = 0; true; star = "**"
+// j = 1; false
+// hasil **
+
+// loop luar 3
+// i = 2;
+// j = 0; true; star = "**"
+// j = 1; true; star = "***"
+// j = 2; false
+// hasil ***
+
+// dst
+for (var i = 5; i > 0; i--) {
+    var star = "";
+    for(var j = 0; j < i; j++) {
+        star = star + "*";
+    }
+    console.log(star);
+}
+
+// *****
+// ****
+// ***
+// **
+// *
